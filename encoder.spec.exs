9d1ecec312c5ccdc35d76f1b7da3264aff0317ee
Vishal Kichloo <31816531+kichloo@users.noreply.github.com>
2019-10-20 09:34:25 +0530

    Created encoder.spec.exs
    Tried to create encoder.spec.exs (I AM A BEGINNER)

---
+++ encoder.spec.exs
@@ -0,0 +1,13 @@
+module encoder.Native
+spec create(
+  channels :: int,
+  sample_rate :: int,
+  aot :: int,
+  bitrate_mode :: int,
+  bitrate :: int
+) :: {:ok :: label, state}
+  | {:error :: label, reason :: atom}
+
+spec encode_frame(payload, state) :: {:ok :: label, payload}
+  | {:error :: label, reason :: atom}
+  | {:error :: label, :no_data :: label}
